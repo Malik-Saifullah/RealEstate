a467c6e5689aa85560a67bc43719abe9044dd280
Malik Saifullah <maliksaifullah282@gmail.com>
2020-12-01 00:40:35 +0500

    adding some sections

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $('.banner__content__box').owlCarousel({
-    items:1,
-    loop:true,
+    items: 1,
+    loop: true,
     nav: true,
-    dots: false
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 2300
 });
