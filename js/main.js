ad1990adeb5d26417a1ac4a340da2a83b3eb9f4b
Malik Saifullah <maliksaifullah282@gmail.com>
2020-12-03 16:10:32 +0500

    adding team section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,24 @@ $('.banner__content__box').owlCarousel({
     autoplay: true,
     autoplayTimeout: 2300
 });
+
+
+$('.agents__slider').owlCarousel({
+    items: 3,
+    loop: true,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 2300,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+});
