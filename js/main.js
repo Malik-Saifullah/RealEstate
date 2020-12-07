4988229be4241f9ea2d02b3b6d80314fca18172f
Malik Saifullah <maliksaifullah282@gmail.com>
2020-12-07 22:42:12 +0500

    adding sticky header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+$(function () { // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function (e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        // Close one dropdown when selecting another
+        $('.nav-dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $('html').click(function () {
+        $('.nav-dropdown').hide();
+    });
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function () {
+        $('nav ul').slideToggle();
+    });
+    // Hamburger to X toggle
+    $('#nav-toggle').on('click', function () {
+        this.classList.toggle('active');
+    });
+}); // end DOM ready
+
+$(".btn-sale").click(function () {
+    $(this).addClass("selected");
+    $(".btn-rent").removeClass("selected");
+    $("#rent-form-container").hide();
+    $("#sale-form-container").css("display", "flex");
+});
+
+$(".btn-rent").click(function () {
+    $(this).addClass("selected");
+    $(".btn-sale").removeClass("selected");
+    $("#sale-form-container").hide();
+    $("#rent-form-container").css("display", "flex");
+});
+
 $('.banner__content__box').owlCarousel({
     items: 1,
     loop: true,
@@ -15,15 +51,15 @@ $('.agents__slider').owlCarousel({
     dots: false,
     // autoplay: true,
     // autoplayTimeout: 2300,
-    responsive:{
-        0:{
-            items:1
+    responsive: {
+        0: {
+            items: 1
         },
-        768:{
-            items:2
+        768: {
+            items: 2
         },
-        992:{
-            items:3
+        992: {
+            items: 3
         }
     }
 });
