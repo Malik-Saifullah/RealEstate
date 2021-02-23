7a3bee30338af9738b774fccd84e5a84ff2d6142
Malik Saifullah <maliksaifullah282@gmail.com>
2021-02-23 15:03:28 +0500

    solve preloading style issue

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 //Hide Loading Box (Preloader)
-function handlePreloader() {
+$(window).on('load', function () {
     if ($('.preloader').length) {
         $('.preloader').delay(200).fadeOut(500);
     }
-}
-$(window).on('load', function () {
-    handlePreloader();
 });
 
 
